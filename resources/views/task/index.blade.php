b3ec8b0e47101ffa211e020c257472488e7d9764
ernie <dy99520@gmail.com>
2020-05-23 23:44:53 +0800

    加入側選Menu

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -1,18 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                @include('task.create.card')
+    <link href="{{ asset('css/sidebar.css') }}" rel="stylesheet">
+    <div class="wrapper">
+
+        @include('task.sidebar')
+
+        <div id="content">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-8">
+                        @include('task.create.card')
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    <br>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                @include('task.show.card', ['tasks' => $tasks])
+            <br>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-8">
+                        @include('task.show.card', ['tasks' => $tasks])
+                    </div>
+                </div>
             </div>
         </div>
     </div>
